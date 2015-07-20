ded2a6ad03f8d3904fe54080580dba039a417605
Ferdi kızıltoprak <FerdikT@gmail.com>
2015-07-20 17:05:44 +0300

    Oluşturuldu.

---
+++ src/main/java/tr/edu/fsm/dersprog/FSMVUDersProg/App.java
@@ -0,0 +1,13 @@
+package tr.edu.fsm.dersprog.FSMVUDersProg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
