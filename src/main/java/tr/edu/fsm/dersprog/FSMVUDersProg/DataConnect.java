e6c1763676d386d8a736e4cc2810e892ed09f8bf
Ferdi kızıltoprak <FerdikT@gmail.com>
2015-07-21 10:54:40 +0300

    Login page çalıştırıldı.

---
+++ src/main/java/tr/edu/fsm/dersprog/FSMVUDersProg/DataConnect.java
@@ -0,0 +1,27 @@
+package tr.edu.fsm.dersprog.FSMVUDersProg;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+ 
+public class DataConnect {
+ 
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://217.199.187.194/", "cl55-dersprog", "wH!x3BbsC");
+            return con;
+        } catch (Exception ex) {
+            System.out.println("Database.getConnection() Error -->"
+                    + ex.getMessage());
+            return null;
+        }
+    }
+ 
+    public static void close(Connection con) {
+        try {
+            con.close();
+        } catch (Exception ex) {
+        }
+    }
+}
